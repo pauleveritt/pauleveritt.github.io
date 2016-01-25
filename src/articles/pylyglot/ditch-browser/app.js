75f9e5f1aa43a5700c214c41fbd5fce7e82227af
Paul Everitt <pauleveritt@me.com>
2016-01-25 14:48:12 -0500

    Get rid of another dead-wood-previous.

--- src/articles/pylyglot/ditch-browser/app.js
+++
@@ -1,7 +0,0 @@
-function dogYears (humanYears) {
-    return humanYears * 7;
-}
-
-if (typeof require !== 'undefined' && require.main === module) {
-    console.log('4 human years gives dog years of', dogYears((4)));
-}
