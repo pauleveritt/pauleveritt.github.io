50abcde9931ddc76cc150010aea4fd7fdbecf9fa
Paul Everitt <pauleveritt@me.com>
2016-02-02 18:06:59 -0500

    Works.

--- src/articles/pylyglot/todo_pythonic_js/app/todo.js
+++ src/articles/pylyglot/todo_pythonic_js/app/todo.js
@@ -1,59 +1,74 @@
 import $ from 'jquery';
-import tmpl from './tmpl';
 
 export default class ToDos {
 
     constructor () {
-        var newName = $('#newName'),
-            todoList = $('#todoList'),
-            template = tmpl('list_todos');
+        this.newName = $('#newName');
+        this.todoList = $('#todoList');
 
-        var todos;
-
-        function refreshToDos () {
-            /* Fetch the list of todos and re-draw the listing */
-            $.get('http://localhost:5000/api/todo', function (data) {
-                todos = data['objects'];
-                todoList.find('ul')
-                    .replaceWith(template({todos: todos}));
-            });
-        }
-
-        // Create a new to do
-        newName.change(function () {
-            var payload = JSON.stringify({name: newName.val()});
-            $.post('http://localhost:5000/api/todo', payload, function () {
-                refreshToDos();
-                newName.val('');
-            })
+        // Event handlers
+        this.newName.change(() => this.create(this.newName.val()));
+        this.todoList.on('click', '.delete', (evt) => {
+            this.delete($(evt.target).closest('li')[0].id);
         });
-
-        // Edit a to do
-        todoList.on('click', '.edit', function () {
-            // Toggle the <input> for this todo
-            todoList.find('li').removeAttr('editing');
-            var li = $(this).closest('li').first().attr('editing', '1');
+        this.todoList.on('click', '.edit', (evt) => {
+            // Hide currently-editing and open this one for editing
+            this.todoList.find('li').removeAttr('editing');
+            $(evt.target).closest('li').first().attr('editing', '1');
         });
-        todoList.on('change', 'input', function () {
+        this.todoList.on('change', 'input', (evt) => {
             // When the revealed-input changes, update using PATCH
-            var todoId = $(this).closest('li')[0].id,
-                data = JSON.stringify({name: $(this).val()});
-            $.ajax({url: 'http://localhost:5000/api/todo/' + todoId, type: 'PATCH', data: data})
-                .done(function () {
-                    refreshToDos();
-                });
+            let todoId = $(evt.target).closest('li')[0].id,
+                data = JSON.stringify({name: $(evt.target).val()});
+            this.update(todoId, data);
         });
 
-        // Delete an existing to do
-        todoList.on('click', '.delete', function () {
-            var todoId = $(this).closest('li')[0].id;
-            $.ajax({url: 'http://localhost:5000/api/todo/' + todoId, type: 'DELETE'})
-                .done(function () {
-                    refreshToDos();
-                });
-        });
 
         // On startup, go fetch the list of todos and re-draw
-        refreshToDos();
+        this.refresh();
+    }
+
+    create (newName) {
+        let payload = JSON.stringify({name: newName});
+        $.post('http://localhost:5000/api/todo', payload, () => {
+            this.refresh();
+            this.newName.val('');
+        })
+    }
+
+    delete (todoId) {
+        $.ajax({url: 'http://localhost:5000/api/todo/' + todoId, type: 'DELETE'})
+            .done(() => this.refresh());
+    }
+
+    update (todoId, data) {
+        $.ajax({url: 'http://localhost:5000/api/todo/' + todoId, type: 'PATCH', data: data})
+            .done(() => this.refresh());
+    }
+
+    renderToDo (todo) {
+        return `
+        <li class="list-group-item" id="${ todo.id }">
+            <span>${ todo.name }</span>
+            <input class="form-control input-sm" title="Edit title"
+                   value="${ todo.name }"/>
+
+            <div class="btn-group pull-right" role="group">
+                <button class="btn btn-xs btn-default edit">Edit</button>
+                <button class="btn btn-xs btn-default delete">Delete</button>
+            </div>
+        </li>
+        `
+    }
+
+    refresh () {
+        $.get('http://localhost:5000/api/todo', (data) => {
+            this.todoList
+                .html(
+                    data['objects']
+                        .map(todo => this.renderToDo(todo))
+                        .join('\n')
+                );
+        });
     }
 };
--- src/articles/pylyglot/todo_pythonic_js/index.rst
+++ src/articles/pylyglot/todo_pythonic_js/index.rst
@@ -12,12 +12,103 @@ Pythonic JavaScript for ToDoMVC
 
     - Change app.js to import class and new ToDo()
 
-    - Move "refreshToDos" -> method "render"
+- newName -> create method
 
-        - var self = this
+    - var newName -> this.newName
 
-        - todos => self.todos
+    - create (newName) {
 
-        - var todos in render()
+    - Cut-and-paste from // Create a new to do
 
-        - template => this.template
+    - let, arrow function, name: newName
+
+    - Demonstrate it failing
+
+- refresh method
+
+    - Cut-and-paste renderToDos
+
+    - Arrow function
+
+    - Get rid of intermediate todos = and paste directly
+
+    - todoList.find -> this.todoList.find
+
+    - template -> this.template
+
+    - Add a this.todoList and this.template
+
+- delete
+
+    - Event handler::
+
+        this.todoList.on('click', '.delete', (evt) => {
+            this.delete($(evt.target).closest('li')[0].id);
+        });
+
+    - Make a delete method, passed in todoId, arrow function
+
+- editing
+
+    - Cut and paste existing handler
+
+    - this.todoList
+
+    - arrow function (evt)
+
+    - $(this) -> $(evt.target)
+
+- change
+
+    - Cut and paste existing handler
+
+    - this.todoList
+
+    - Arrow function evt
+
+    - $(this) -> $(evt.target) in *both* places
+
+    - var -> let
+
+    - Move $.ajax to method, leaving behind this.update(todoId, data)
+
+        - .done(() => this.refresh());
+
+- Use ES6 template strings
+
+    - Add a renderToDo(todo) method
+
+    - return string literal
+
+    - Copy <li> contents into it
+
+    - Replace todos[i] with todo
+
+    - Change 3 syntax to ${}
+
+    - Change render()
+
+        - this.todoList.html()
+
+        - Inside there
+
+        - data['objects'].map(todo => this.renderTodo(todo))
+
+        - .join('\n')
+
+
+- Get rid of template strings
+
+    - Remove <script> template from index.html
+
+    - <ul id="todoList" class="list-group"></ul>
+
+    - Remove tmpl.js
+
+    - Remove import
+
+    - Remove this.tmpl
+
+    - Remove function refreshToDos
+
+    - Call this.render instead of refreshTodos
